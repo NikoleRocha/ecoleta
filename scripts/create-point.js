e473ab49e4c1ae55c4115542131bdd5c6e3bf580
Nikole <kolenick7n@hotmail.com>
2021-12-04 22:58:56 -0300

    página cadastro front finalizada

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -39,3 +39,34 @@ function getCities(event) {
 document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities)
+
+//Itens de coleta
+const itensToCollect = document.querySelectorAll(".itens-grid li")
+
+for (let item of itensToCollect) {
+  item.addEventListener("click", handleSelecteditem)
+}
+
+let selectedItems = []
+const collectedItems = document.querySelector("input[name=items]")
+
+function handleSelecteditem(event) {
+  const itemLi = event.target
+  itemLi.classList.toggle("selected")
+  const itemId = event.target.dataset.id
+
+  const alreadySelected = selectedItems.findIndex(item => {
+    return item == itemId
+  })
+
+  if (alreadySelected >= 0) {
+    const filteredItems = selectedItems.filter(item => {
+      return item != itemId
+    })
+    selectedItems = filteredItems
+  } else {
+    selectedItems.push(itemId)
+  }
+
+  collectedItems.value = selectedItems
+}
